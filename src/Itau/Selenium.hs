3129450f9387e2160024ab39183c16bf279f7739
pedropbazzo <32115702+pedropbazzo@users.noreply.github.com>
2019-05-18 21:05:37 -0300

    Itau lib

---
+++ src/Itau/Selenium.hs
@@ -0,0 +1,146 @@
+module Itau.Selenium
+    ( seleniumRun,
+      Navegador (FF, CH),
+      sleep,
+      waitElemFound,
+      clickAndWait,
+      tryClickAndWait,
+      tryClickAndWaitElem
+    ) where
+
+import           Control.Concurrent
+import           Control.Exception
+import           Control.Monad
+import           Control.Monad.IO.Class
+import           Data.Aeson.Types 
+import qualified Data.ByteString as B
+import           Data.List
+import           Data.Text (pack)
+import           GHC.IO.Handle
+import           Network.HTTP
+import           Network.URI (parseURI)
+import           System.Directory
+import           System.Environment.Executable
+import           System.Process
+import           Test.WebDriver
+import           Test.WebDriver.Class
+import           Test.WebDriver.Commands.Wait
+import           Test.WebDriver.Firefox.Profile
+data Navegador = FF | CH
+
+sleep :: Int -> IO()
+sleep msecs =
+    threadDelay $ msecs * 1000
+
+waitElemFound :: (MonadIO wd, WebDriver wd) => Selector -> wd Element
+waitElemFound sel =
+    waitUntil 30 $ findElem sel
+
+clickAndWait :: (MonadIO wd, WebDriver wd) => wd Element -> wd ()
+clickAndWait el = do
+    el >>= click
+    liftIO $ sleep 3000
+    return ()
+
+tryClickAndWaitElem :: (MonadIO wd, WebDriver wd) => Selector -> wd ()
+tryClickAndWaitElem el = tryClickAndWait $ findElem el
+
+tryClickAndWait :: (MonadIO wd, WebDriver wd) => wd Element -> wd ()
+tryClickAndWait el = waitUntil 30 $ clickAndWait el
+
+myFFProfile :: String -> IO (PreparedProfile Firefox)
+myFFProfile dir =        
+        prepareProfile $ 
+             addPref "browser.download.useDownloadDir"               True
+           $ addPref "browser.download.dir"                          dir
+           $ addPref "browser.download.folderList"                   (2 :: Integer)
+           $ addPref "browser.download.manager.alertOnEXEOpen"       False
+           $ addPref "browser.download.manager.closeWhenDone"        True
+           $ addPref "browser.download.manager.focusWhenStarting"    False
+           $ addPref "browser.download.manager.showAlertOnComplete"  False
+           $ addPref "browser.download.manager.showWhenStarting"     False
+           $ addPref "browser.download.manager.useWindow"            False
+           $ addPref "browser.download.panel.shown"                  False
+           $ addPref "browser.helperApps.alwaysAsk.force"            False
+           -- sim, o Itau informa o content type como "content/type" :/
+           $ addPref "browser.helperApps.neverAsk.saveToDisk"        ("content/type,text/plain" :: String)
+           $ addPref "xpinstall.signatures.required"                 False
+             defaultProfile
+
+myConfig :: Navegador -> String -> IO WDConfig
+myConfig CH dir =
+    return $ useBrowser chrome {
+            chromeOptions = ["--start-maximized"]
+        }
+        def
+    where
+        def = defaultConfig {
+            wdCapabilities = defaultCaps {
+                additionalCaps = [
+                    ("download.default_directory", String $ pack dir), 
+                    ("directory_upgrade", Bool True),
+                    ("extensions_to_open", String $ pack "")
+                    ]
+                }
+        }
+myConfig FF dir = do
+    pprof <- myFFProfile dir
+    return $ defaultConfig {
+        wdCapabilities = defaultCaps {
+            additionalCaps = [("marionette", Bool True)],
+            browser = firefox {
+                ffProfile = Just  pprof
+            }
+        }
+    }
+
+checkAndDownloadSelenium :: FilePath -> IO ()
+checkAndDownloadSelenium fname = do
+        fExists <- doesFileExist fname
+        unless  fExists $ do
+            putStrLn "Selenium server não encontrado. Iniciando download..."
+            bytes <- simpleHTTP (defaultGETRequest_ url) >>= getResponseBody
+            B.writeFile fname bytes
+            putStrLn "Download terminado"
+    where
+        (Just url) = parseURI "http://selenium-release.storage.googleapis.com/2.53/selenium-server-standalone-2.53.0.jar"
+
+seleniumRun :: Navegador -> String -> WD a -> IO ()
+seleniumRun nav dir action = 
+    seleniumRun' `catch` exHandler `finally` seleniumStop
+    where 
+        exHandler :: SomeException -> IO ()
+        exHandler e = putStrLn $ "Erro durante a execução" ++ displayException e
+        seleniumRun' :: IO ()
+        seleniumRun' = do
+            seleniumStart
+            config <- myConfig nav dir
+            _ <- runSession config $ finallyClose $ do
+                setImplicitWait 100
+                _ <- action
+                return ()
+            return ()
+
+seleniumStart :: IO ()
+seleniumStart = do
+        (path, _) <- splitExecutablePath
+        let fname = fullFilePath path
+        checkAndDownloadSelenium fname
+        (_, _, Just st_err, _) <- createProcess (shell $ "java -jar " ++ fname){std_out = CreatePipe, std_err = CreatePipe}
+        putStrLn "Iniciando Selenium Server"
+        waitStart st_err
+    where
+        -- hardcoded, por enquanto, com a versão 2.53
+        bname = "selenium-server-standalone-2.53.0.jar"
+        fullFilePath path = path ++ bname
+        waitStart fstream = do
+            ln <- hGetLine fstream
+            unless ("INFO - Selenium Server is up and running" `isSuffixOf` ln) $ waitStart fstream
+
+seleniumStop :: IO ()
+seleniumStop = do
+        putStrLn "Desligando Selenium Server"
+        res <- simpleHTTP (defaultGETRequest_ url) >>= getResponseBody ::IO String
+        assert (res == "OKOK") $ return ()
+    where
+        (Just url) = parseURI "http://localhost:4444/selenium-server/driver/?cmd=shutDownSeleniumServer"
---
+++ src/Itau/Utils.hs
@@ -0,0 +1,93 @@
+module Itau.Utils (
+      withEcho
+    , getUserInput
+    , today
+    , comb
+    , formatDayMMMYYYY
+    , fracEquals
+    , fracEquals2
+    , fromItauNum
+    , fromItauDate
+    , isBlank
+    , ItauDateDirection (Forward, Backward, Current)
+) where
+
+import           Control.Exception
+import           Control.Monad
+import qualified Data.Foldable as F
+import           Data.List.Split
+import qualified Data.Text as T
+import           Data.Time.Clock
+import           Data.Time.LocalTime
+import           Data.Time.Calendar
+import           Data.Text (Text)
+import           GHC.IO.Handle
+import           System.IO
+
+isBlank :: Text -> Bool
+isBlank = T.null . T.strip
+
+today :: IO Day
+today = do
+    now <- getCurrentTime >>= utcToLocalZonedTime
+    return $ localDay $ zonedTimeToLocalTime now
+
+comb :: Int -> [a] -> [[a]]
+comb m xs = combsBySize xs !! m
+    where
+        combsBySize = foldr partial ([[]] : repeat [])
+        partial x next = zipWith (++) (map (map (x:)) ([]:next)) next
+
+withEcho :: Bool -> IO a -> IO a
+withEcho echo action = do
+    old <- hGetEcho stdin
+    bracket_ (hSetEcho stdin echo) (hSetEcho stdin old) action
+
+getUserInput :: String -> Bool -> IO String
+getUserInput text echo = do
+    putStr text
+    hFlush stdout
+    ret <- withEcho echo getLine
+    unless echo $ putChar '\n'
+    return ret
+
+fracEquals :: Float -> Float  -> Float  -> Bool
+fracEquals maxDiff a b  = abs (a - b) <= maxDiff
+
+fracEquals2 :: Float -> Float  -> Bool
+fracEquals2 = fracEquals 0.01
+
+fromItauNum :: Text -> Float
+fromItauNum val =
+        if '-' `elem` T.unpack val
+            then negate num
+            else num
+    where
+        el = (`F.elem`  (",0123456789":: String))
+        num = read . T.unpack . T.replace "," "." $ T.filter el val
+
+
+data ItauDateDirection = Forward | Current | Backward
+
+yearGivenDirection :: Integer -> Int -> Int -> ItauDateDirection -> Integer
+yearGivenDirection curYear _        _     Current  = curYear
+yearGivenDirection curYear curMonth month Forward  = if month < curMonth then curYear + 1 else curYear
+yearGivenDirection curYear curMonth month Backward = if month > curMonth then curYear - 1 else curYear
+
+fromItauDate :: Day -> ItauDateDirection -> Text -> Day
+fromItauDate day direction t =
+    pDate $ splitOn "/" (T.unpack t)
+    where
+        (year, month, _) = toGregorian day
+        pDate [d, m] =
+            fromGregorian (yearGivenDirection year month (read m) direction) (read m) (read d)
+        pDate [d, m, y] = fromGregorian (read y) (read m) (read d)
+        pDate x = error $ show x
+
+formatDayMMMYYYY :: Day -> String        
+formatDayMMMYYYY d = 
+    let (y, m, _) = toGregorian d in 
+    show y ++ "-" ++ (meses !! (m - 1))
+    where 
+        meses = ["JAN", "FEV", "MAR", "ABR", "MAI", "JUN", "JUL", "AGO", "SET", "OUT", "NOV", "DEZ"]
+            
